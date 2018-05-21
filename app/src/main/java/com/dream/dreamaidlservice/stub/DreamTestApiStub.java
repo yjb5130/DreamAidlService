90fa682fd4aaa586f46e7d7cbdf1d30855ea95db
yjb5130 <yjb5130@163.com>
2018-05-21 15:59:54 +0800

    添加读取与写入byte值的aidl方法。

--- app/src/main/java/com/dream/dreamaidlservice/stub/DreamTestApiStub.java
+++ app/src/main/java/com/dream/dreamaidlservice/stub/DreamTestApiStub.java
@@ -7,6 +7,7 @@ import com.dream.platform.IDreamTestAidl;
 public class DreamTestApiStub extends IDreamTestAidl.Stub {
 
     private int mAidlInt = 0;
+    private byte mAidlByte = 0;
 
     @Override
     public int getAidlIntValue() throws RemoteException {
@@ -16,5 +17,15 @@ public class DreamTestApiStub extends IDreamTestAidl.Stub {
     @Override
     public void setAidlIntValue(int value) throws RemoteException {
         mAidlInt = value;
+    }
+
+    @Override
+    public byte getAidlByteValue() throws RemoteException {
+        return mAidlByte;
+    }
+
+    @Override
+    public void setAidlByteValue(byte value) throws RemoteException {
+        mAidlByte = value;
     }
 }
