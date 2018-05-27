1e1e8dfd24f3e9c04c01cf142b88c4ec632c5be5
yjb5130 <yjb5130@163.com>
2018-05-27 21:13:48 +0800

    添加callback的函数，返回服务器的回调。

--- app/src/main/java/com/dream/platform/stub/DreamSDKManagerStub.java
+++ app/src/main/java/com/dream/platform/stub/DreamSDKManagerStub.java
@@ -1,25 +1,58 @@
 package com.dream.platform.stub;
 
 import android.content.Context;
+import android.os.RemoteCallbackList;
 import android.os.RemoteException;
+import android.util.Log;
 
+import com.dream.platform.IDreamCallbackListener;
 import com.dream.platform.IDreamSDKManager;
 import com.dream.platform.IDreamTestAidl;
 
 public class DreamSDKManagerStub extends IDreamSDKManager.Stub {
 
+    private final static String TAG = "DreamSDKManagerStub";
+
     private Context mContext;
 
     private DreamTestApiStub mDreamTestApiStub;
+    private final RemoteCallbackList<IDreamCallbackListener> mCallback = new RemoteCallbackList<>();
 
     public DreamSDKManagerStub(Context context) {
         mContext = context;
     }
 
+    synchronized public void doCallbackListener(int status) {
+        final int N = mCallback.beginBroadcast();
+        for (int i = 0; i < N; i++) {
+            try {
+                mCallback.getBroadcastItem(i).notifyMessage(status);
+                Log.d(TAG, "packageName: " + mCallback.getBroadcastItem(i).toString());
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+        mCallback.finishBroadcast();
+    }
+
+    @Override
+    public void registerCallbackListener(IDreamCallbackListener listener) throws RemoteException {
+        if (listener != null) {
+            mCallback.register(listener);
+        }
+    }
+
+    @Override
+    public void unregisterCallbackListener(IDreamCallbackListener listener) throws RemoteException {
+        if (listener != null) {
+            mCallback.unregister(listener);
+        }
+    }
+
     @Override
     public IDreamTestAidl getDreamTestApi() throws RemoteException {
         if (mDreamTestApiStub == null) {
-            mDreamTestApiStub = new DreamTestApiStub();
+            mDreamTestApiStub = new DreamTestApiStub(this);
         }
         return mDreamTestApiStub;
     }
--- app/src/main/java/com/dream/platform/stub/DreamTestApiStub.java
+++ app/src/main/java/com/dream/platform/stub/DreamTestApiStub.java
@@ -7,11 +7,17 @@ import com.dream.platform.parcel.EntityBaseParcel;
 
 public class DreamTestApiStub extends IDreamTestAidl.Stub {
 
+    private DreamSDKManagerStub mSdkManager;
+
     private int mAidlInt = 0;
     private byte mAidlByte = 0;
     private String mAidlString = null;
     private byte[] mAidlBytes = null;
     private EntityBaseParcel mAidlBaseItem = null;
+
+    public DreamTestApiStub(DreamSDKManagerStub sdkManager) {
+        mSdkManager = sdkManager;
+    }
 
     @Override
     public int getAidlIntValue() throws RemoteException {
@@ -21,6 +27,9 @@ public class DreamTestApiStub extends IDreamTestAidl.Stub {
     @Override
     public void setAidlIntValue(int value) throws RemoteException {
         mAidlInt = value;
+        if (mSdkManager != null) {
+            mSdkManager.doCallbackListener(1);
+        }
     }
 
     @Override
@@ -31,6 +40,9 @@ public class DreamTestApiStub extends IDreamTestAidl.Stub {
     @Override
     public void setAidlByteValue(byte value) throws RemoteException {
         mAidlByte = value;
+        if (mSdkManager != null) {
+            mSdkManager.doCallbackListener(2);
+        }
     }
 
     @Override
@@ -41,6 +53,9 @@ public class DreamTestApiStub extends IDreamTestAidl.Stub {
     @Override
     public void setAidlString(String string) throws RemoteException {
         mAidlString = string;
+        if (mSdkManager != null) {
+            mSdkManager.doCallbackListener(3);
+        }
     }
 
     @Override
@@ -51,6 +66,9 @@ public class DreamTestApiStub extends IDreamTestAidl.Stub {
     @Override
     public void setAidlBytes(byte[] value) throws RemoteException {
         mAidlBytes = value;
+        if (mSdkManager != null) {
+            mSdkManager.doCallbackListener(4);
+        }
     }
 
     @Override
@@ -61,5 +79,8 @@ public class DreamTestApiStub extends IDreamTestAidl.Stub {
     @Override
     public void setBaseParcel(EntityBaseParcel item) throws RemoteException {
         mAidlBaseItem = item;
+        if (mSdkManager != null) {
+            mSdkManager.doCallbackListener(5);
+        }
     }
 }
